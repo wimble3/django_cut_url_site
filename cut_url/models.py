ce305d243577e798a31c68705c73823d3c029a80
Sergey Avdeev <wimble@internet.ru>
2023-02-10 12:04:29 +0500

    check commit

--- cut_url/models.py
+++ cut_url/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class ShortUrl(models.Model):
     url = models.CharField(max_length=1000)
     short_url = models.CharField(max_length=255, unique=True)
+
--- cut_url/views.py
+++ cut_url/views.py
@@ -15,37 +15,6 @@ host = ALLOWED_HOSTS[0]
 port = '8000'
 
 
-def index(request):
-    short_url = ''
-
-    if request.method == 'POST':
-        form = AddShortUrl(request.POST)
-
-        if form.is_valid():
-            url = request.POST['url']
-
-            if url_is_valid(url):
-                try:
-                    row = ShortUrl.objects.get(url=url)
-                    short_url = row.short_url
-                except:
-                    row = ShortUrl(url=url, short_url=generateShortUrl())
-                    row.save()
-                    short_url = row.short_url
-            else:
-                short_url = 'Invalid URL!'
-    else:
-        form = AddShortUrl()
-
-    context = {
-        'title': 'Cut url',
-        'menu': menu,
-        'form': form,
-        'short_url': short_url,
-    }
-    return render(request, 'cut_url/index.html', context=context)
-
-
 def generateShortUrl() -> str:
     token = uuid.uuid4()
     return host + ':' + port + '/' + str(token)[:8]
